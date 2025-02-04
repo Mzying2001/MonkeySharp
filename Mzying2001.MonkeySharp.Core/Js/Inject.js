6c96307795e546e3fd6a7520cf744c829d43bd14
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-02-04 11:26:19 +0800

    Update __MonkeySharp_ApiParam

--- Mzying2001.MonkeySharp.Core/Js/Inject.js
+++ Mzying2001.MonkeySharp.Core/Js/Inject.js
@@ -6,15 +6,19 @@
 
     // the parameter send to C#
     class __MonkeySharp_ApiParam {
-        constructor(scriptId, param) {
-            this.scriptId = scriptId;
-            this.jsonData = JSON.stringify(param);
+        constructor(param) {
+            if (typeof param === "undefined") {
+                throw new Error("param is required");
+            } else {
+                this.scriptId = __MonkeySharp_CurrentScriptId;
+                this.jsonData = JSON.stringify(param);
+            }
         }
     }
 
     // logs a message to the console
     function GM_log(message) {
-        __MonkeySharp.sendMsg("GM_log", new __MonkeySharp_ApiParam(__MonkeySharp_CurrentScriptId, message));
+        __MonkeySharp.sendMsg("GM_log", new __MonkeySharp_ApiParam(message));
     }
 
     /*==========REPLACE_CODE_HERE==========*/
