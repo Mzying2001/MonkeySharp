6e764bce89f4f6b9f85079242b583d19624a89fc
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-02-11 20:53:23 +0800

    Define `__MonkeySharp` object in IIFE

--- Mzying2001.MonkeySharp.Core/Js/Init.js
+++ Mzying2001.MonkeySharp.Core/Js/Init.js
@@ -1,88 +1,5 @@
 // Init.js
 
-var __MonkeySharp =
-{
-    internalProps: [
-        "__MonkeySharp",
-        "__MonkeySharp_Injected",
-        "__MonkeySharp_Messenger",
-        "__MonkeySharp_AsyncMessenger"
-    ],
-
-    sendMsg: function (msg, param) {
-        // __MonkeySharp_Messenger: injected by MonkeySharp
-        if (typeof __MonkeySharp_Messenger !== "undefined") {
-            var result = __MonkeySharp_Messenger.sendMessage([msg, this.serialize(param)]);
-            return this.deserialize(result);
-        } else {
-            this.consoleLog("Messenger is not found.");
-            return undefined;
-        }
-    },
-
-    sendMsgAsync: async function (msg, param) {
-        // __MonkeySharp_AsyncMessenger: injected by MonkeySharp
-        if (typeof __MonkeySharp_AsyncMessenger !== "undefined") {
-            //Task<T> is not supported by default in JavaScript, so we use sendMessage rather than sendMessageAsync.
-            var result = await __MonkeySharp_AsyncMessenger.sendMessage([msg, this.serialize(param)]);
-            return this.deserialize(result);
-        } else {
-            this.consoleLog("AsyncMessenger is not found.");
-            return undefined;
-        }
-    },
-
-    serialize: function (obj) {
-        if (typeof obj === "undefined") {
-            return null;
-        } else {
-            return JSON.stringify(obj);
-        }
-    },
-
-    deserialize: function (str) {
-        if (typeof str === "undefined" || str === null) {
-            return undefined;
-        } else {
-            return JSON.parse(str);
-        }
-    },
-
-    consoleLog: function (msg, isJson) {
-        if (typeof isJson === "undefined" || isJson === false) {
-            console.log("[MonkeySharp]", msg);
-        } else {
-            console.log("[MonkeySharp]", this.deserialize(msg));
-        }
-    },
-
-    onDocumentStart: function () {
-        this.sendMsg("document-start", window.location.href);
-    },
-
-    onDocumentBody: function () {
-        this.sendMsg("document-body", window.location.href);
-    },
-
-    onDocumentEnd: function () {
-        this.sendMsg("document-end", window.location.href);
-    },
-
-    onDocumentIdle: function () {
-        this.sendMsg("document-idle", window.location.href);
-    },
-
-    onContextMenu: function () {
-        this.sendMsg("context-menu", window.location.href);
-    },
-
-    raiseUrlChangeEvent: function () {
-        var event = new Event("urlchange");
-        window.dispatchEvent(event);
-        if (typeof window.onurlchange === "function") window.onurlchange(event);
-    }
-};
-
 (function () {
     // avoid duplicate injection
     if (window.__MonkeySharp_Injected) {
@@ -91,6 +8,89 @@ var __MonkeySharp =
 
     // mark as injected
     window.__MonkeySharp_Injected = true;
+
+    // MonkeySharp object
+    window.__MonkeySharp = {
+        internalProps: [
+            "__MonkeySharp",
+            "__MonkeySharp_Injected",
+            "__MonkeySharp_Messenger",
+            "__MonkeySharp_AsyncMessenger"
+        ],
+
+        sendMsg: function (msg, param) {
+            // __MonkeySharp_Messenger: injected by MonkeySharp
+            if (typeof __MonkeySharp_Messenger !== "undefined") {
+                var result = __MonkeySharp_Messenger.sendMessage([msg, this.serialize(param)]);
+                return this.deserialize(result);
+            } else {
+                this.consoleLog("Messenger is not found.");
+                return undefined;
+            }
+        },
+
+        sendMsgAsync: async function (msg, param) {
+            // __MonkeySharp_AsyncMessenger: injected by MonkeySharp
+            if (typeof __MonkeySharp_AsyncMessenger !== "undefined") {
+                //Task<T> is not supported by default in JavaScript, so we use sendMessage rather than sendMessageAsync.
+                var result = await __MonkeySharp_AsyncMessenger.sendMessage([msg, this.serialize(param)]);
+                return this.deserialize(result);
+            } else {
+                this.consoleLog("AsyncMessenger is not found.");
+                return undefined;
+            }
+        },
+
+        serialize: function (obj) {
+            if (typeof obj === "undefined") {
+                return null;
+            } else {
+                return JSON.stringify(obj);
+            }
+        },
+
+        deserialize: function (str) {
+            if (typeof str === "undefined" || str === null) {
+                return undefined;
+            } else {
+                return JSON.parse(str);
+            }
+        },
+
+        consoleLog: function (msg, isJson) {
+            if (typeof isJson === "undefined" || isJson === false) {
+                console.log("[MonkeySharp]", msg);
+            } else {
+                console.log("[MonkeySharp]", this.deserialize(msg));
+            }
+        },
+
+        onDocumentStart: function () {
+            this.sendMsg("document-start", window.location.href);
+        },
+
+        onDocumentBody: function () {
+            this.sendMsg("document-body", window.location.href);
+        },
+
+        onDocumentEnd: function () {
+            this.sendMsg("document-end", window.location.href);
+        },
+
+        onDocumentIdle: function () {
+            this.sendMsg("document-idle", window.location.href);
+        },
+
+        onContextMenu: function () {
+            this.sendMsg("context-menu", window.location.href);
+        },
+
+        raiseUrlChangeEvent: function () {
+            var event = new Event("urlchange");
+            window.dispatchEvent(event);
+            if (typeof window.onurlchange === "function") window.onurlchange(event);
+        }
+    };
 
     // support urlchange event
     window.onurlchange = null;
